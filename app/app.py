d3176ed508a3061e1dcd525facf743dee45d6530
Huniiiiiii <rtg8464@gmail.com>
2025-05-20 15:19:08 +0900

    [fix] app.py 수정

--- app/app.py
+++ app/app.py
@@ -1,8 +1,12 @@
-from flask import Flask,render_template
-from routes.modify import register_routes
+from flask import Flask, render_template
+from routes.modify import register_routes as register_modify
+from routes.dashboard import register_routes as register_dashboard
 
 app = Flask(__name__)
-register_routes(app)
+
+# 각 라우터 등록
+register_modify(app)
+register_dashboard(app)
 
 @app.route('/')
 def home():
