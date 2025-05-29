f040c9b147f7264a62747f8cf26bc8a6593bcf73
KimSeHun <rtg8464@gmail.com>
2025-05-29 12:05:06 +0900

    Delete update_column.sql

--- update_column.sql
+++
@@ -1,8 +0,0 @@
-
--- uesrs 테이블에 열 추가하는 query 입니다. 
-ALTER TABLE users ADD COLUMN fullname varchar(100) NOT NULL;
-
--- 이건 users의 컬럼에 해당하는 값을 수정하는 query 입니다 id는 데이터 저장에 따라 바꿔야 합니다..
--- UPDATE users SET name ='kim@naver.com' where id = 1;
--- UPDATE users SET fullname ='kim' where id = 1;
-
